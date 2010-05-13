513a6a4316d8595c5bc36e87255050e747c3349a
Erik Price <erik.price16@gmail.com>
2010-05-13 18:52:45 -0400

    Cleaned up a bit, increased line width

--- src/scribe/core.clj
+++ src/scribe/core.clj
@@ -1,56 +1,54 @@
 (ns scribe.core
-  (:import (java.awt Frame Dimension Graphics)
+  (:import (java.awt Frame Dimension Graphics Graphics2D BasicStroke RenderingHints Color)
 	   (javax.swing JFrame JPanel)
 	   (javax.swing.event MouseInputAdapter)
-	   (java.awt.event MouseMotionAdapter MouseEvent MouseListener MouseAdapter MouseWheelListener MouseWheelEvent)))
+	   (java.awt.event  MouseEvent MouseListener MouseAdapter MouseWheelListener MouseWheelEvent)))
 
-;;TODO: MAKE STUFF DISPLAY IN RIGHT SPOT!
-
-;;for REPL use only!
-;(add-classpath "file:/usr/share/java/jogl.jar")
-;(add-classpath "file:/usr/share/java/gluegen-rt.jar")
+;;TODO: ALLOW FOR SCREEN CLEAR
 
 
 (defstruct Point :x :y)
 (defstruct Line :b :e)
 
 (def last-point (ref nil))
+(def last-line (ref nil))
 
-(def lines (ref []))
 
-(defn draw-line [#^Graphics g lines] 
-  (if (seq lines)
-    (let [line (first lines)]
-      (if (not (nil? line))
-	(let [bx (:x (line :b))
-	      by (:y (line :b))
-	      ex (:x (line :e))
-	      ey (:y (line :e))]
-	  (doto g
-	    (.drawLine bx by ex ey)))))))
-;	  (draw-line g (rest lines)))))))
-
-(defn draw-lines [#^Graphics g]
-  (draw-line g @lines))
+(defn draw-line [#^Graphics2D g line]
+  (if (not (nil? line))
+    (let [bx (:x (line :b))
+	  by (:y (line :b))
+	  ex (:x (line :e))
+	  ey (:y (line :e))]
+      (doto g
+	(.setStroke (new BasicStroke 5.0))
+	(.setRenderingHint RenderingHints/KEY_ANTIALIASING
+			    RenderingHints/VALUE_ANTIALIAS_ON)
+	(.drawLine bx by ex ey)))))
 
 (def canvas (proxy [JPanel] []
-	      (paintComponent [#^Graphics g]
-			      (proxy-super paintComponent g)
-			      (draw-lines g))))
+		   (paintComponent [#^Graphics g]
+				   (draw-line g @last-line))))
 
 (def mouse-handle
      (proxy [MouseInputAdapter] []
        
        (mousePressed [#^MouseEvent e]
-		     (dosync (ref-set last-point (struct Point (.getX e) (.getY e)))))
+		     (dosync
+		      (let [ex (.getX e)
+			    ey (.getY e)]
+			(ref-set last-point (struct Point ex ey))))
+		     (.repaint canvas))
        
        (mouseDragged [#^MouseEvent e]
 		     (if (not (nil? @last-point))
-		       (let [x (@last-point :x)
-			     y (@last-point :y)]
+		       (let [bx (@last-point :x)
+			     by (@last-point :y)
+			     ex (.getX e)
+			     ey (.getY e)]
 			 (dosync
-			  (ref-set last-point (struct Point (.getX e) (.getY e)))
-			  (alter lines into [(struct Line (struct Point x y)  @last-point)]))
+			  (ref-set last-point (struct Point ex ey))
+			  (ref-set last-line (struct Line (struct Point bx by) @last-point)))
 			 (.repaint canvas))))
        
        (mouseReleased [#^MouseEvent e])))
